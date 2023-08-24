de1bb9e1364442a4d8ab432e37343b4c2a8f8ad5
MinYoungWI <young1y@nate.com>
2023-08-24 23:20:27 +0900

    axios-이모지 조회, 이모지 추가, 날짜 클릭 후 todo,게시글 조회

--- src/components/calendar/CalendarFunc.js
+++ src/components/calendar/CalendarFunc.js
@@ -2,18 +2,25 @@ import React, { useState } from "react";
 import Calendar from "react-calendar";
 import "react-calendar/dist/Calendar.css";
 import moment from "moment";
-import EmojiPicker, { Emoji } from "emoji-picker-react";
+import EmojiPicker from "emoji-picker-react";
 import "./CalendarFunc.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { BiMehBlank } from "react-icons/bi";
+import axios from "axios";
 
-function CalendarFunc() {
+function CalendarFunc({ sendDataToParent }) {
+  const accessToken =
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI2NGU2OWE2Y2VmYTZmNjdiZjc0MTZhYzAiLCJpYXQiOjE2OTI4MzQ0NTQsImV4cCI6MTcwMDYxMDQ1NH0.IXDlGN3E_OmlKteegULvlDtMsyb_wF59_vJgH6LJuww";
   const [value, onChange] = useState(new Date());
   const [clickedDate, setClickedDate] = useState(); // 선택한 날짜를 저장할 상태
   const [selectedEmoji, setSelectedEmoji] = useState(); //선택한 이모지를 저장할 상태
   const [showPicker, setShowPicker] = useState(false);
   const [dateList, setDateList] = useState([]);
   const [emojiList, setEmojiList] = useState([]);
+  const activeDate = moment(value).format("YYYYMMDD"); // 클릭한 날짜 (년-월-일))
+  const endOfMonth = moment(activeDate).endOf("month").format("YYYYMMDD"); //클릭한 날짜 달의 마지막날짜
+  const startOfMonth = moment(activeDate).startOf("month").format("YYYYMMDD"); //매월 1일
+
   //이모지 클릭하면 나타나게 하는 함수
   const onEmojiClick = (emojiObject, e) => {
     setSelectedEmoji(emojiObject.emoji);
@@ -23,14 +30,48 @@ function CalendarFunc() {
   const selectedEmojiSave = selectedEmoji ? selectedEmoji : <BiMehBlank />;
   //클라이언트가 클릭한 날짜 clickedDate에 저장
   const saveDate = (date) => {
-    const clickedDate = moment(date).format("YYYYMMDD");
-    setClickedDate(clickedDate);
+    const onClickDayClickedDate = moment(date).format("YYYYMMDD");
+    setClickedDate(onClickDayClickedDate);
+    // 클릭한 날짜를 상위 컴포넌트로 전달
+    sendDataToParent(onClickDayClickedDate);
   };
+
+  //기존에 저장된 이모지 get요청
+  axios.get("http://34.64.151.119/api/days/imogies", {
+    params: {
+      startDate: startOfMonth,
+      endDate: endOfMonth,
+    },
+    headers: {
+      Authorization: `Bearer ${accessToken}`,
+    },
+  });
+
   // 클라이언트에서 선택한 날짜와 이모지를 서버로 전송
-  const sendDataToServer = (clickedDate, selectedEmoji) => {
+  const sendDataToServer = async (clickedDate, selectedEmoji) => {
     if (clickedDate && selectedEmoji) {
       setDateList((prevDateList) => [...prevDateList, clickedDate]); //중복값 저장 가능
       setEmojiList((prevEmojiList) => [...prevEmojiList, selectedEmoji]);
+      // POST 요청 전송
+      const test = {
+        date: clickedDate,
+        emoji: selectedEmoji,
+      };
+      try {
+        const response = await axios.patch(
+          "http://34.64.151.119/api/days",
+          test,
+          {
+            headers: {
+              Authorization: `Bearer ${accessToken}`,
+            },
+          }
+        );
+        console.log(clickedDate);
+        console.log(response.data); //응답 데이터를 설정
+      } catch (error) {
+        console.error(error);
+      }
     }
   };
 
@@ -49,7 +90,7 @@ function CalendarFunc() {
     return <div>{EmojiDateAdded}</div>;
   };
 
-  console.log(dateList, emojiList);
+  // console.log(dateList, emojiList);
   return (
     <div className="wrap" style={{ float: "left" }}>
       <div className="EmojiSelection">
--- src/routes/HOME/Home.js
+++ src/routes/HOME/Home.js
@@ -1,14 +1,35 @@
-import React from "react";
-import CalendarFunc from "../../components/calendar/CalendarFunc";
-import Header from "../../components/header/Header.js";
+import React, { useState } from "react";
+import Header from "../../components/header/Header";
+import CalendarFunc from "../../components/calendar/CalendarFunc.js";
 import DiaryHome from "../../components/diary/DiaryHome";
 import TodoComponent from "../../components/todo/TodoComponent";
+import axios from "axios";
 
 function Home() {
+  // 상위 컴포넌트의 상태와 상태 변경 함수
+  const [clickedDate, setClickedDate] = useState();
+  // 하위 컴포넌트로 전달할 함수
+  const handleDataFromCalendarFunc = (data) => {
+    setClickedDate(data); // 받은 데이터를 상태에 업데이트
+  };
+
+  const accessToken =
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI2NGU2OWE2Y2VmYTZmNjdiZjc0MTZhYzAiLCJpYXQiOjE2OTI4MzQ0NTQsImV4cCI6MTcwMDYxMDQ1NH0.IXDlGN3E_OmlKteegULvlDtMsyb_wF59_vJgH6LJuww";
+  axios.get("http://34.64.151.119/api/days/records", {
+    params: {
+      date: clickedDate,
+    },
+    headers: {
+      Authorization: `Bearer ${accessToken}`,
+    },
+  });
+
+  console.log(clickedDate);
   return (
     <div>
       <Header></Header>
-      <CalendarFunc></CalendarFunc>
+      {/* 하위 컴포넌트에 함수를 props로 전달 */}
+      <CalendarFunc sendDataToParent={handleDataFromCalendarFunc} />
       <TodoComponent></TodoComponent>
       <DiaryHome></DiaryHome>
     </div>
